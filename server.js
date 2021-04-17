c3d3ff0860b83d3137ff0921a9cadaf07bff1eb2
Scott Wells <efwells101@gmail.com>
2021-04-17 00:32:30 -0700

    Grab data from endpoint and display to front end

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+// Load the entire local directory root
 app.use(express.static(__dirname))
 
 const messages = [
@@ -8,10 +9,12 @@ const messages = [
     { name: 'Al', message: 'Steve!' }
 ]
 
+// GET request
 app.get('/messages', (req, res)=>{
     res.send(messages)
 })
 
+// Make server and listen on port 3000
 let server = app.listen(3000, () => {
     console.log('Server is running on ', server.address().port)
 })
